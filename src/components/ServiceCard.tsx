5722da07503d47a132b2146d8a28ba65a28f1a48
KTennakoon <110615349+keshariT99@users.noreply.github.com>
2025-06-21 18:59:26 +0530

    update service card

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -13,12 +13,12 @@ const ServiceCard = ({ icon, title, description }: ServiceCardProps) => {
       <h3 className="text-xl font-semibold mb-4">{title}</h3>
       <p className="text-gray-400 mb-6">{description}</p>
       <div className="flex gap-4">
-        <button className="text-gray-400 hover:text-[#39FF14] transition-colors">
+        {/**<button className="text-gray-400 hover:text-[#39FF14] transition-colors">
           Read More
         </button>
         <button className="text-[#00E7FF] hover:text-[#32E512] transition-colors">
           Get started
-        </button>
+        </button>**/}
       </div>
     </div>
   );
