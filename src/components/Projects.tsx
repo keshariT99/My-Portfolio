9ddf4d6b6ac520e1c405f553eda79123527ec4ff
KTennakoon <110615349+keshariT99@users.noreply.github.com>
2025-07-07 02:02:20 +0530

    update projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -85,7 +85,7 @@ const Projects = () => {
     },
   ];
 
-  const filters = ['All', 'Web', 'UI/UX', 'Case Study'];
+  const filters = ['All', 'Web', 'UI/UX'];
 
   const filteredProjects = activeFilter === 'All' 
     ? projects 
